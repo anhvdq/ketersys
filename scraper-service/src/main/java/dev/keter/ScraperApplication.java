61c814ddcc1e701db26d0c4ecc21a5b86aeab742
Quoc Anh <41427319+pinkeraw@users.noreply.github.com>
2023-03-09 09:17:09 +0000

    add scheduler

--- scraper-service/src/main/java/dev/keter/ScraperApplication.java
+++ scraper-service/src/main/java/dev/keter/ScraperApplication.java
@@ -3,10 +3,12 @@ package dev.keter;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
 public class ScraperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScraperApplication.class, args);
